458a3f88e9715620d4e72fb866c2672a5fad3b29
Emily Bolten <44903354+ebolten@users.noreply.github.com>
2019-09-25 12:42:27 -0400

    Add files via upload

---
+++ EBolten_Project2_GasMileage/EBolten_Project2_GasMileage/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace EBolten_Project2_GasMileage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtBoxInput = new System.Windows.Forms.RichTextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblExplain = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.pictureZodiac = new System.Windows.Forms.PictureBox();
+            this.lblis = new System.Windows.Forms.Label();
+            this.pictureDragon = new System.Windows.Forms.PictureBox();
+            this.pictureDog = new System.Windows.Forms.PictureBox();
+            this.pictureHorse = new System.Windows.Forms.PictureBox();
+            this.pictureMonkey = new System.Windows.Forms.PictureBox();
+            this.pictureOx = new System.Windows.Forms.PictureBox();
+            this.picturePig = new System.Windows.Forms.PictureBox();
+            this.pictureRabbit = new System.Windows.Forms.PictureBox();
+            this.pictureRat = new System.Windows.Forms.PictureBox();
+            this.pictureRooster = new System.Windows.Forms.PictureBox();
+            this.pictureSheep = new System.Windows.Forms.PictureBox();
+            this.pictureSnake = new System.Windows.Forms.PictureBox();
+            this.pictureTiger = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZodiac)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDragon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHorse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMonkey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureOx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRabbit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRooster)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSheep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSnake)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTiger)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtBoxInput
+            // 
+            this.TxtBoxInput.Location = new System.Drawing.Point(46, 142);
+            this.TxtBoxInput.Name = "TxtBoxInput";
+            this.TxtBoxInput.Size = new System.Drawing.Size(177, 34);
+            this.TxtBoxInput.TabIndex = 0;
+            this.TxtBoxInput.Text = "";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(235, 142);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 34);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblExplain
+            // 
+            this.lblExplain.AutoSize = true;
+            this.lblExplain.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExplain.Location = new System.Drawing.Point(41, 26);
+            this.lblExplain.Name = "lblExplain";
+            this.lblExplain.Size = new System.Drawing.Size(269, 29);
+            this.lblExplain.TabIndex = 2;
+            this.lblExplain.Text = "Get Your Zodiac Sign!";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(40, 271);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 32);
+            this.lblResult.TabIndex = 3;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(42, 106);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(209, 23);
+            this.lblYear.TabIndex = 4;
+            this.lblYear.Text = "Enter Your Birth Year:";
+            // 
+            // pictureZodiac
+            // 
+            this.pictureZodiac.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureZodiac.ErrorImage")));
+            this.pictureZodiac.Image = ((System.Drawing.Image)(resources.GetObject("pictureZodiac.Image")));
+            this.pictureZodiac.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureZodiac.InitialImage")));
+            this.pictureZodiac.Location = new System.Drawing.Point(351, 12);
+            this.pictureZodiac.Name = "pictureZodiac";
+            this.pictureZodiac.Size = new System.Drawing.Size(272, 251);
+            this.pictureZodiac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureZodiac.TabIndex = 5;
+            this.pictureZodiac.TabStop = false;
+            this.pictureZodiac.Click += new System.EventHandler(this.pictureZodiac_Click);
+            // 
+            // lblis
+            // 
+            this.lblis.AutoSize = true;
+            this.lblis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblis.Location = new System.Drawing.Point(30, 203);
+            this.lblis.Name = "lblis";
+            this.lblis.Size = new System.Drawing.Size(251, 25);
+            this.lblis.TabIndex = 6;
+            this.lblis.Text = "Your Zodiac Sign is The:";
+            // 
+            // pictureDragon
+            // 
+            this.pictureDragon.Image = ((System.Drawing.Image)(resources.GetObject("pictureDragon.Image")));
+            this.pictureDragon.Location = new System.Drawing.Point(164, 258);
+            this.pictureDragon.Name = "pictureDragon";
+            this.pictureDragon.Size = new System.Drawing.Size(100, 64);
+            this.pictureDragon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureDragon.TabIndex = 7;
+            this.pictureDragon.TabStop = false;
+            this.pictureDragon.Visible = false;
+            // 
+            // pictureDog
+            // 
+            this.pictureDog.Image = ((System.Drawing.Image)(resources.GetObject("pictureDog.Image")));
+            this.pictureDog.Location = new System.Drawing.Point(123, 258);
+            this.pictureDog.Name = "pictureDog";
+            this.pictureDog.Size = new System.Drawing.Size(100, 64);
+            this.pictureDog.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureDog.TabIndex = 8;
+            this.pictureDog.TabStop = false;
+            this.pictureDog.Visible = false;
+            // 
+            // pictureHorse
+            // 
+            this.pictureHorse.Image = ((System.Drawing.Image)(resources.GetObject("pictureHorse.Image")));
+            this.pictureHorse.Location = new System.Drawing.Point(181, 258);
+            this.pictureHorse.Name = "pictureHorse";
+            this.pictureHorse.Size = new System.Drawing.Size(100, 64);
+            this.pictureHorse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureHorse.TabIndex = 9;
+            this.pictureHorse.TabStop = false;
+            this.pictureHorse.Visible = false;
+            // 
+            // pictureMonkey
+            // 
+            this.pictureMonkey.Image = ((System.Drawing.Image)(resources.GetObject("pictureMonkey.Image")));
+            this.pictureMonkey.Location = new System.Drawing.Point(164, 258);
+            this.pictureMonkey.Name = "pictureMonkey";
+            this.pictureMonkey.Size = new System.Drawing.Size(100, 64);
+            this.pictureMonkey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureMonkey.TabIndex = 10;
+            this.pictureMonkey.TabStop = false;
+            this.pictureMonkey.Visible = false;
+            // 
+            // pictureOx
+            // 
+            this.pictureOx.Image = ((System.Drawing.Image)(resources.GetObject("pictureOx.Image")));
+            this.pictureOx.Location = new System.Drawing.Point(110, 258);
+            this.pictureOx.Name = "pictureOx";
+            this.pictureOx.Size = new System.Drawing.Size(100, 64);
+            this.pictureOx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureOx.TabIndex = 11;
+            this.pictureOx.TabStop = false;
+            this.pictureOx.Visible = false;
+            // 
+            // picturePig
+            // 
+            this.picturePig.Image = ((System.Drawing.Image)(resources.GetObject("picturePig.Image")));
+            this.picturePig.Location = new System.Drawing.Point(123, 258);
+            this.picturePig.Name = "picturePig";
+            this.picturePig.Size = new System.Drawing.Size(100, 64);
+            this.picturePig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picturePig.TabIndex = 12;
+            this.picturePig.TabStop = false;
+            this.picturePig.Visible = false;
+            // 
+            // pictureRabbit
+            // 
+            this.pictureRabbit.Image = ((System.Drawing.Image)(resources.GetObject("pictureRabbit.Image")));
+            this.pictureRabbit.Location = new System.Drawing.Point(181, 258);
+            this.pictureRabbit.Name = "pictureRabbit";
+            this.pictureRabbit.Size = new System.Drawing.Size(100, 64);
+            this.pictureRabbit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureRabbit.TabIndex = 13;
+            this.pictureRabbit.TabStop = false;
+            this.pictureRabbit.Visible = false;
+            // 
+            // pictureRat
+            // 
+            this.pictureRat.Image = ((System.Drawing.Image)(resources.GetObject("pictureRat.Image")));
+            this.pictureRat.Location = new System.Drawing.Point(110, 258);
+            this.pictureRat.Name = "pictureRat";
+            this.pictureRat.Size = new System.Drawing.Size(100, 64);
+            this.pictureRat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureRat.TabIndex = 14;
+            this.pictureRat.TabStop = false;
+            this.pictureRat.Visible = false;
+            // 
+            // pictureRooster
+            // 
+            this.pictureRooster.Image = ((System.Drawing.Image)(resources.GetObject("pictureRooster.Image")));
+            this.pictureRooster.Location = new System.Drawing.Point(181, 258);
+            this.pictureRooster.Name = "pictureRooster";
+            this.pictureRooster.Size = new System.Drawing.Size(100, 64);
+            this.pictureRooster.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureRooster.TabIndex = 15;
+            this.pictureRooster.TabStop = false;
+            this.pictureRooster.Visible = false;
+            // 
+            // pictureSheep
+            // 
+            this.pictureSheep.Image = ((System.Drawing.Image)(resources.GetObject("pictureSheep.Image")));
+            this.pictureSheep.Location = new System.Drawing.Point(164, 258);
+            this.pictureSheep.Name = "pictureSheep";
+            this.pictureSheep.Size = new System.Drawing.Size(100, 64);
+            this.pictureSheep.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureSheep.TabIndex = 16;
+            this.pictureSheep.TabStop = false;
+            this.pictureSheep.Visible = false;
+            // 
+            // pictureSnake
+            // 
+            this.pictureSnake.Image = ((System.Drawing.Image)(resources.GetObject("pictureSnake.Image")));
+            this.pictureSnake.Location = new System.Drawing.Point(164, 258);
+            this.pictureSnake.Name = "pictureSnake";
+            this.pictureSnake.Size = new System.Drawing.Size(100, 64);
+            this.pictureSnake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureSnake.TabIndex = 17;
+            this.pictureSnake.TabStop = false;
+            this.pictureSnake.Visible = false;
+            // 
+            // pictureTiger
+            // 
+            this.pictureTiger.Image = ((System.Drawing.Image)(resources.GetObject("pictureTiger.Image")));
+            this.pictureTiger.Location = new System.Drawing.Point(164, 258);
+            this.pictureTiger.Name = "pictureTiger";
+            this.pictureTiger.Size = new System.Drawing.Size(100, 64);
+            this.pictureTiger.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureTiger.TabIndex = 18;
+            this.pictureTiger.TabStop = false;
+            this.pictureTiger.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(626, 369);
+            this.Controls.Add(this.pictureTiger);
+            this.Controls.Add(this.pictureSnake);
+            this.Controls.Add(this.pictureSheep);
+            this.Controls.Add(this.pictureRooster);
+            this.Controls.Add(this.pictureRat);
+            this.Controls.Add(this.pictureRabbit);
+            this.Controls.Add(this.picturePig);
+            this.Controls.Add(this.pictureOx);
+            this.Controls.Add(this.pictureMonkey);
+            this.Controls.Add(this.pictureHorse);
+            this.Controls.Add(this.pictureDog);
+            this.Controls.Add(this.pictureDragon);
+            this.Controls.Add(this.lblis);
+            this.Controls.Add(this.pictureZodiac);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblExplain);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.TxtBoxInput);
+            this.Name = "Form1";
+            this.Text = "Zodiac Sign";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureZodiac)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDragon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHorse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMonkey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureOx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRabbit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRooster)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSheep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureSnake)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTiger)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TxtBoxInput;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblExplain;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.PictureBox pictureZodiac;
+        private System.Windows.Forms.Label lblis;
+        private System.Windows.Forms.PictureBox pictureDragon;
+        private System.Windows.Forms.PictureBox pictureDog;
+        private System.Windows.Forms.PictureBox pictureHorse;
+        private System.Windows.Forms.PictureBox pictureMonkey;
+        private System.Windows.Forms.PictureBox pictureOx;
+        private System.Windows.Forms.PictureBox picturePig;
+        private System.Windows.Forms.PictureBox pictureRabbit;
+        private System.Windows.Forms.PictureBox pictureRat;
+        private System.Windows.Forms.PictureBox pictureRooster;
+        private System.Windows.Forms.PictureBox pictureSheep;
+        private System.Windows.Forms.PictureBox pictureSnake;
+        private System.Windows.Forms.PictureBox pictureTiger;
+    }
+}
+
---
+++ EBolten_Project2_GasMileage/EBolten_Project2_GasMileage/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EBolten_Project2_GasMileage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string input = TxtBoxInput.Text;
+            int numInput = System.Convert.ToInt32(input);
+            int zodiac = numInput % 12;
+            string result;
+
+            switch (zodiac)
+            {
+                case 0:
+                    result = "Monkey";
+                    pictureMonkey.Visible = true;
+
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+
+                    break;
+                case 1:
+                    result = "Rooster";
+                    pictureRooster.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 2:
+                    result = "Dog";
+                    pictureDog.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 3:
+                    result = "Pig";
+                    picturePig.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 4:
+                    result = "Rat";
+                    pictureRat.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 5:
+                    result = "Ox";
+                    pictureOx.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 6:
+                    result = "Tiger";
+                    pictureTiger.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 7:
+                    result = "Rabbit";
+                    pictureRabbit.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 8:
+                    result = "Dragon";
+                    pictureDragon.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 9:
+                    result = "Snake";
+                    pictureSnake.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureHorse.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 10:
+                    result = "Horse";
+                    pictureHorse.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureSheep.Visible = false;
+                    break;
+                case 11:
+                    result = "Sheep";
+                    pictureSheep.Visible = true;
+
+                    pictureMonkey.Visible = false;
+                    pictureRooster.Visible = false;
+                    pictureDog.Visible = false;
+                    picturePig.Visible = false;
+                    pictureRat.Visible = false;
+                    pictureOx.Visible = false;
+                    pictureTiger.Visible = false;
+                    pictureRabbit.Visible = false;
+                    pictureDragon.Visible = false;
+                    pictureSnake.Visible = false;
+                    pictureHorse.Visible = false;
+                    break;
+                default:
+                    result = "Invalid Year";
+                    break;
+            }
+
+            lblResult.Text = result + "!";
+        }
+
+        private void pictureZodiac_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
